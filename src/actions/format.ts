9cc649c1d78b1faeb48fa5df5e5c0c5512e5f7fc
Godruoyi <godruoyi@gmail.com>
2025-01-16 19:43:06 +0800

    wip: cannot load sharp module

--- src/actions/format.ts
+++ src/actions/format.ts
@@ -1,41 +1,31 @@
 import { Config, Image, Input, Output } from "../types";
-import fs from "fs";
-import sharp from "sharp";
 import path from "path";
+import sharp from "sharp";
 
-export default async function(
+export default async function (
   i: Input,
   config: Config,
   _services: Record<string, Config>,
-  originImage: Image
+  originImage: Image,
 ): Promise<Output> {
   const format = config?.["format"] as string;
-  const options = config?.["options"] as
-    | sharp.OutputOptions
-    | sharp.JpegOptions
-    | sharp.PngOptions
-    | sharp.WebpOptions
-    | sharp.AvifOptions
-    | sharp.HeifOptions
-    | sharp.JxlOptions
-    | sharp.GifOptions
-    | sharp.Jp2Options
-    | sharp.TiffOptions;
+  const options = config?.["options"] as sharp.JpegOptions | sharp.PngOptions | sharp.WebpOptions | sharp.AvifOptions;
 
   // const image = fs.createReadStream(i.image.value);
   const newPath = path.join(
     path.dirname(originImage.value),
-    path.basename(originImage.value, path.extname(originImage.value)) + "." + format?.toLocaleString()
+    path.basename(originImage.value, path.extname(originImage.value)) + "." + format?.toLocaleString(),
   );
-
-  await sharp(i.image.value)
-    .toFormat(format as keyof sharp.FormatEnum, options)
-    .toFile(newPath);
 
   console.log("Image has been formatted to", format, "and saved to", newPath);
 
+  await sharp(originImage.value)
+    .toFormat(format as keyof sharp.FormatEnum, options)
+    .toFile(newPath);
+
   return i as Output;
 
+  // todo: fix cannot find module sharp even though it is installed
   // const writeStream = fs.createWriteStream(originImage.value);
   // const transformer = sharp().toFormat(format as keyof sharp.FormatEnum, options);
   //
