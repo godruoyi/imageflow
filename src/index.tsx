86d762018ec6f5afe73126495950e9b78b95d7f3
Godruoyi <godruoyi@gmail.com>
2025-02-17 13:03:02 +0800

    support show progressing in raycast detail

---
+++ src/index.tsx
@@ -0,0 +1,51 @@
+import { Detail } from "@raycast/api";
+import { usePromise } from "@raycast/utils";
+import React, { useRef, useState } from "react";
+import { Input, WorkflowAlias } from "./types";
+import { getSelectedImages } from "./supports/image";
+import { getWorkflowConfigs } from "./supports/workflow";
+import { createWorkflow } from "./workflow";
+import { showError } from "./supports/error";
+
+type Props = {
+  arguments: {
+    workflow?: WorkflowAlias;
+  };
+};
+
+export default function Index(props: Props) {
+  const [markdown, setMarkdown] = useState("");
+  const hasRun = useRef(false);
+
+  usePromise(async () => {
+    if (hasRun.current) {
+      return;
+    }
+
+    hasRun.current = true;
+    await run(setMarkdown, props.arguments.workflow);
+  }, []);
+
+  return <Detail markdown={markdown} />;
+}
+
+async function run(
+  setMarkdown: React.Dispatch<React.SetStateAction<string>>,
+  workflowAlias?: WorkflowAlias,
+): Promise<string> {
+  try {
+    const images = await getSelectedImages();
+    const configs = await getWorkflowConfigs();
+    const workflowName = workflowAlias || ("default" as WorkflowAlias);
+    const workflow = await createWorkflow(configs, workflowName, setMarkdown);
+
+    await Promise.all(images.map((image) => workflow.run(image as Input)));
+  } catch (e) {
+    console.log("-----");
+    console.log(e);
+    console.log("-----");
+    await showError(e);
+  }
+
+  return "";
+}
